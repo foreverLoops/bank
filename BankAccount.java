2335bb8e5a432ff56a214b971a3446fe8161d402
Bradwill Van Niekerk <137079341+Bradwill-Van-Niekerk@users.noreply.github.com>
2025-02-19 13:41:56 +0200

    basic

---
+++ BankAccount.java
@@ -0,0 +1,46 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+            return true;
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+            return false;
+        }
+    }
+
+    public boolean transfer(BankAccount toAccount, double amount) {
+        if (this.withdraw(amount)) {
+            toAccount.deposit(amount);
+            System.out.println("Transferred: $" + amount + " to Account: " + toAccount.getAccountNumber());
+            return true;
+        }
+        return false;
+    }
+}
